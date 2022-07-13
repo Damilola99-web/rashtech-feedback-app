4e4a62ff3345598494f18050804a7fb3af51032c
Abdul-rasheed <rasheedw622@gmail.com>
2022-07-13 19:09:22 +0100

    edited the comment section

--- src/components/CommentData.js
+++ src/components/CommentData.js
@@ -3,23 +3,28 @@ import React, { useState } from 'react';
 export default function CommentData({ comment }) {
 	const [ replyOpen, setReplyOpen ] = useState(false);
 	return (
-		<div key={comment.id} className=" w-full flex flex-row justify-center border-b-2 my-3 p-4 space-x-8">
-			<img src={comment.photoUrl} alt="" className=" rounded-full w-12 h-12" />
+		<div key={comment.id} className=" w-full flex flex-row justify-center border-b-2 my-3 p-4">
 			<div className=" w-full flex flex-col justify-center space-y-6 mb-4">
 				<div className=" w-full flex items-center justify-between">
-					<div className=" flex flex-col justify-center">
-						<p className=" text-base text-blue-800 font-bold">{comment.name}</p>
-						<p>{comment.username}</p>
+					<div className=' flex flex-row space-x-6 justify-between items-center'>
+						<img src={comment.photoUrl} alt="" className=" rounded-full w-12 h-12" />
+						<div className=" flex flex-col justify-center">
+							<p className=" text-base text-blue-800 font-bold">{comment.name}</p>
+							<p>{comment.username}</p>
+						</div>
 					</div>
 					<p className=" cursor-pointer text-blue-600" onClick={() => setReplyOpen(true)}>
 						Reply
 					</p>
 				</div>
-				<p className=" text-blue-800">{comment.content}</p>
+				<p className=" text-gray-500">{comment.content}</p>
 				{replyOpen && (
-					<form className=' flex flex-col md:flex-row justify-between space-y-4 md:space-y-0'>
-						<textarea placeholder={`Reply to ${comment.username}`} className=' w-full md:w-[75%] h-[70px] rounded-lg bg-[#f2f4ff] p-3 focus:outline-blue-600' />
-						<div className=' w-full md:w-[23%] md:max-w-[200px] flex flex-row md:flex-col space-x-3 md:space-x-0 md:space-y-3'>
+					<form className=" flex flex-col md:flex-row justify-between space-y-4 md:space-y-0">
+						<textarea
+							placeholder={`Reply to ${comment.username}`}
+							className=" w-full md:w-[75%] h-[70px] rounded-lg bg-[#f2f4ff] p-3 focus:outline-blue-600"
+						/>
+						<div className=" w-full md:w-[23%] md:max-w-[200px] flex flex-row md:flex-col space-x-3 md:space-x-0 md:space-y-3">
 							<span
 								onClick={() => setReplyOpen(false)}
 								className=" cursor-pointer justify-center flex items-center w-full  px-6 h-[50px] text-base  font-bold text-white rounded-lg bg-red-600"
