e1867a8cb0e45d6d9f42538ada406af6b33715c4
Abdul-rasheed <rasheedw622@gmail.com>
2022-07-13 14:04:42 +0100

    liking and unlike done

--- src/components/CommentData.js
+++ src/components/CommentData.js
@@ -8,12 +8,33 @@ export default function CommentData({ comment }) {
 			<div className=" w-full flex flex-col justify-center space-y-6 mb-4">
 				<div className=" w-full flex items-center justify-between">
 					<div className=" flex flex-col justify-center">
-						<p className=' text-base text-blue-800 font-bold'>{comment.name}</p>
+						<p className=" text-base text-blue-800 font-bold">{comment.name}</p>
 						<p>{comment.username}</p>
 					</div>
-					<p className=' text-blue-600'>Reply</p>
+					<p className=" cursor-pointer text-blue-600" onClick={() => setReplyOpen(true)}>
+						Reply
+					</p>
 				</div>
-                <p className=' text-blue-800'>{comment.content}</p>
+				<p className=" text-blue-800">{comment.content}</p>
+				{replyOpen && (
+					<form className=' flex flex-col md:flex-row justify-between space-y-4 md:space-y-0'>
+						<textarea placeholder={`Reply to ${comment.username}`} className=' w-full md:w-[75%] h-[70px] rounded-lg bg-[#f2f4ff] p-3 focus:outline-blue-600' />
+						<div className=' w-full md:w-[23%] md:max-w-[200px] flex flex-row md:flex-col space-x-3 md:space-x-0 md:space-y-3'>
+							<span
+								onClick={() => setReplyOpen(false)}
+								className=" cursor-pointer justify-center flex items-center w-full  px-6 h-[50px] text-base  font-bold text-white rounded-lg bg-red-600"
+							>
+								Cancel
+							</span>
+							<span
+								type="submit"
+								className=" cursor-pointer justify-center flex items-center w-full s px-6 h-[50px] text-base  font-bold text-white rounded-lg bg-[#ad1fea]"
+							>
+								Post reply
+							</span>
+						</div>
+					</form>
+				)}
 			</div>
 		</div>
 	);
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,42 +1,64 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Icon from '../assets/shared/icon-comments.svg';
-import Arrow from '../assets/shared/icon-arrow-up.svg'
-import Down from '../assets/shared/icon-arrow-down.svg'
+import Arrow from '../assets/shared/icon-arrow-up.svg';
+import Down from '../assets/shared/icon-arrow-down.svg';
+import { useFirestore } from '../hooks/useFirestore';
 
 export default function Product({ product }) {
+	const { updateDocument } = useFirestore('feedbacks');
 	const { id, category, comments, title, upvotes, description } = product;
+	const handleLike = async () => {
+		let update = {liked : true};
+		if (product.liked) {
+			update = { liked: false, upvotes: upvotes - 1 };
+		}
+		else {
+			update = { liked: true, upvotes: upvotes + 1 };
+		}
+		await updateDocument(id, update)
+	};
 	return (
-		<Link to={`/details/${id}`}>
+		<div to={`/details/${id}`}>
 			{product && (
 				<div className="w-full rounded-lg bg-white my-2 p-4 flex px-8 md:px-8 justify-between items-baseline sm:items-center space-x-8 ">
-					<div className='flex flex-col-reverse sm:flex-row '>
-						<button onClick={()=>alert('clicked')} className=" my-2 flex-row sm:w-[40px] flex px-4 py-2 sm:flex-col space-x-2 max-w-[78px] h-auto items-center justify-center sm:space-y-2 sm:space-x-0 sm:h-[55px] rounded-lg bg-[#f2f4ff] text-blue-600 font-bold sm:mx-1  bottom-0 left-0 ">
-							<img src={Arrow} alt="" />
+					<div className="flex flex-col-reverse sm:flex-row ">
+						<button
+							onClick={() => handleLike()}
+							className={
+								!product.liked ? (
+									' my-2 flex-row sm:w-[40px] flex px-4 py-2 sm:flex-col space-x-2 max-w-[78px] h-auto items-center justify-center sm:space-y-2 sm:space-x-0 sm:h-[55px] rounded-lg bg-[#f2f4ff] text-blue-600 font-bold sm:mx-1  bottom-0 left-0 '
+								) : (
+									'bg-blue-600 text-[#f2f4ff]  my-2 flex-row sm:w-[40px] flex px-4 py-2 sm:flex-col space-x-2 max-w-[78px] h-auto items-center justify-center sm:space-y-2 sm:space-x-0 sm:h-[55px] rounded-lg font-bold sm:mx-1  bottom-0 left-0 '
+								)
+							}
+						>
+							{!product.liked && <img src={Arrow} alt="" />}
+							{product.liked && <img src={Down} alt="" />}
 							<p>{upvotes}</p>
 						</button>
-						<div className=" sm:ml-10">
-							<h3 className=' text-base font-bold text-blue-600 my-1'>{title}</h3>
-							<p className=' text-base my-1'>{description}</p>
+						<Link to={`/details/${id}`} className=" sm:ml-10">
+							<h3 className=" text-base font-bold text-blue-600 my-1">{title}</h3>
+							<p className=" text-base my-1">{description}</p>
 							<button className=" px-5 py-2 flex items-center h-[30px] rounded-lg bg-[#f2f4ff] text-blue-600 font-bold my-2">
 								{category}
 							</button>
-						</div>
+						</Link>
 					</div>
 					{comments && (
-						<div className="flex items-center justify-center space-x-3">
+						<Link to={`/details/${id}`} className="flex items-center justify-center space-x-3">
 							<img src={Icon} alt="" />
 							<p>{comments.length}</p>
-						</div>
+						</Link>
 					)}
 					{!comments && (
-						<div className=" flex items-center justify-center space-x-3">
+						<Link to={`/details/${id}`} className=" flex items-center justify-center space-x-3">
 							<img src={Icon} alt="" />
 							<p>0</p>
-						</div>
+						</Link>
 					)}
 				</div>
 			)}
-		</Link>
+		</div>
 	);
 }
